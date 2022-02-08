9fcf50c8829b0ef05908d96df541d71552feda10
ehsan2793 <ehsan2793@gmail.com>
2022-02-08 03:24:31 -0800

    private

--- main.py
+++ main.py
@@ -10,58 +10,58 @@ class CreditCard():
         limit :credit card limit (in dollars)
         """
         self._balance = 0
-        self._cutomer  = cutomer
-        self._bank = bank
-        self._account = account
-        self._limit = limit
+        self.__cutomer  = cutomer
+        self.__bank = bank
+        self.__account = account
+        self.__limit = limit
 
     def get_customer(self):
         """ Return the name fo the customer """
-        return self._cutomer
+        return self.__cutomer
 
     def get_bank(self):
         """Return the name of the bank"""
 
-        return self._bank
+        return self.__bank
 
     def get_account(self):
         """Return account number """
-        return self._account
+        return self.__account
 
     def get_limit(self):
         """Return the credit card limit"""
-        return self._limit
+        return self.__limit
 
     def get_balance(self):
         """Return the credit card balance"""
-        return self._balance
+        return self.__balance
 
     def charge(self,amount):
         """
         Charge the credit card the amount when credit card limit is suffient
         Return True when charge was successful and False otherwise
         """
-        if amount + self._balance > self._limit:  #if charge would exceed the limit
+        if amount + self.__balance > self.__limit:  #if charge would exceed the limit
             return False   #charge is denied
 
         else:
-            self._balance += amount
+            self.__balance += amount
             return True
 
 
     def make_payment(self,amount):
         """Process the payment that reduce the balance of the credit card"""
-        self._balance -= amount
+        self.__balance -= amount
 
 
 
 
-#
-# help(CreditCard)
+#help(CreditCard)
 
 
-cc = CreditCard('Ehsan Rahimi' , "bank1",'1234 1234 1234 1234',1000) # instance of CreditCard has 5 instance variables
-cc.get_balance()
-cc.get_limit()
-cc.make_payment(100)
-cc.get_balance()
+cc = CreditCard('Ehsan Rahimi' , "bank1",'1234 1234 1232 1234',1000) # instance of CreditCard has 5 instance variables
+
+
+cc.get_account()
+cc.get_bank()
+cc._
