815dfff5ba26c1199704963c75b76c8b647b83da
ehsan2793 <ehsan2793@gmail.com>
2022-02-07 22:28:33 -0800

    init the class

---
+++ main.py
@@ -0,0 +1,20 @@
+class CreditCard():
+    """Custom credit card"""
+    def __init__(self,cutomer,bank,account,limit):
+        """
+        blance is zero at the start
+        cutomer:  the name if the cutomer (ex: 'john anderson')
+        bank : the name of the bank (ex: 'people back')
+        account : account number (ex: "4121 1233 1234 1314")
+        limit :credit card limit (in dollars)
+        """
+        self._balance = 0
+        self._cutomer  = cutomer
+        self._bank = bank
+        self._account = account
+        self._limit = limit
+
+
+
+
+help(CreditCard)
