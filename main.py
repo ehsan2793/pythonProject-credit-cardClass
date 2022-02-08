72a8202e417d8e98eee57d19fa295cdf8f0eb012
ehsan2793 <ehsan2793@gmail.com>
2022-02-07 22:52:40 -0800

    more method added

--- main.py
+++ main.py
@@ -18,6 +18,41 @@ class CreditCard():
         """ Return the name fo the customer """
         return self._cutomer
 
+    def get_bank(self):
+        """Return the name of the bank"""
+
+        return self._bank
+
+    def get_account(self):
+        """Return account number """
+        return self._account
+
+    def get_limit(self):
+        """Return the credit card limit"""
+        return self._limit
+
+    def get_balance(self):
+        """Return the credit card balance"""
+        return self._balance
+
+    def charge(self,amount):
+        """
+        Charge the credit card the amount when credit card limit is suffient
+        Return True when charge was successful and False otherwise
+        """
+        if amount + self._balance > self._limit:  #if charge would exceed the limit
+            return False   #charge is denied
+
+        else:
+            self._balance += amount
+            return True
+
+
+    def make_payment(self,amount):
+        """Process the payment that reduce the balance of the credit card"""
+        self._balance -= amount
+
+
 
 
 
