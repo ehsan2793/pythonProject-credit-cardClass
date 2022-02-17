7ceaab974101187e07ce681ff42a7ffe98638aa4
ehsan2793 <ehsan2793@gmail.com>
2022-02-17 01:38:17 -0800

    game compelete

--- black-jack.py
+++ black-jack.py
@@ -7,7 +7,7 @@ def calculate_score(cards):
         return 0
     if 11 in cards and sum(cards) > 21:
         cards.append(1)
-        arr.remove(11)
+        cards.remove(11)
     return sum(cards)
 
 def deal_cards():
@@ -15,6 +15,23 @@ def deal_cards():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
     card = random.choice(cards)
     return card
+
+def compare(user_score , computer_score):
+    if user_score ==  computer_score:
+        return "Draw "
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "You Won"
+    elif user_score > 21:
+        return "You went over 21, you lose"
+    elif computer_score > 21:
+        return "You Won, computer has over 21"
+
+    elif user_score > computer_score:
+        return "You Won"
+    else:
+        return "You lose"
 
 
 user_card = []
@@ -43,3 +60,12 @@ while not is_game_over:
             user_card.append(deal_cards())
         else:
             is_game_over = True
+
+
+while computer_score != 0  and computer_score < 17:
+    computer_card.append(deal_cards())
+    computer_score = calculate_score(computer_card)
+
+print(f"Your final hand: {user_card}, final score: {user_score}")
+print(f"Computer's cards: {computer_card}, final score: {computer_score}")
+compare(user_score,computer_score)
