8950f28b3b9579b577e1eef0cb4152aece158224
ehsan2793 <ehsan2793@gmail.com>
2022-02-17 23:36:43 -0800

    GNG

---
+++ guess_number.py
@@ -0,0 +1,51 @@
+import random
+logo ="""
+
+░██████╗░███╗░░██╗░██████╗░
+██╔════╝░████╗░██║██╔════╝░
+██║░░██╗░██╔██╗██║██║░░██╗░
+██║░░╚██╗██║╚████║██║░░╚██╗
+╚██████╔╝██║░╚███║╚██████╔╝
+░╚═════╝░╚═╝░░╚══╝░╚═════╝░
+
+"welcome to the Guess Number Game(GNG).
+
+"""
+
+print(logo)
+
+def gng():
+    print("Im think of a number between 1 and 100.")
+    difficulty = input("choose a difficulty. Type 'easy' or 'hard': ")
+
+    random_number = random.randint(1,100)
+
+    if difficulty == "easy":
+        numer_of_choices = 10
+
+    elif difficulty == "hard":
+        numer_of_choices = 5
+
+    while numer_of_choices > 0 :
+        print(f"you have {numer_of_choices} remaining to guess the number.")
+        guess = int(input('make a guess: '))
+
+        if guess == random_number:
+            print("You won")
+            numer_of_choices= 0
+
+        elif guess > random_number:
+            print("Too High")
+            print("Guess again")
+
+        else:
+            print("Too Low")
+            print("Guess again")
+
+        numer_of_choices-= 1
+
+while input("Do you want to play GNG: 'yes' or 'no': ") == "yes":
+    gng()
+
+
+    
